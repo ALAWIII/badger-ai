618a82f5c717d091d131ccfc99b7d831503bab7c
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-28 19:15:57 +0300

    feat: add copy text/html buttons.

---
+++ components/copyButtons.vue
@@ -0,0 +1,24 @@
+<template>
+    <div
+        id="copy-buttons-container"
+        class="flex flex-row justify-evenly rounded-2xl w-full h-[15%] p-1"
+    >
+        <!-- copy buttons -->
+        <div
+            id="copy-text"
+            class="bg-black rounded-l-2xl border text-white w-full h-full hover:bg-amber-50/20"
+        >
+            <button class="w-full h-full">
+                <v-icon name="md-contentcopy-round" class="h-[70%] w-[70%]" />
+            </button>
+        </div>
+        <div
+            id="copy-html"
+            class="bg-black rounded-r-2xl border text-white w-full h-full hover:bg-amber-50/20"
+        >
+            <button class="w-full h-full">
+                <v-icon name="la-html5" class="h-[70%] w-[70%]" />
+            </button>
+        </div>
+    </div>
+</template>
