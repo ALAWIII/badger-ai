96a1002fe0bf2fef381a2a82687b0788105315f1
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-28 14:12:54 +0300

    feat: using shadowdoom it positions the icon to the top left.

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,32 @@
+import popupicon from "@/components/iconPopup.vue";
 export default defineContentScript({
-  matches: ["*://*.google.com/*"],
-  main() {
-    console.log("Hello content.");
+  matches: ["<all_urls>"],
+
+  async main(ctx) {
+    console.log("content script loaded");
+    const ui = await createShadowRootUi(ctx, {
+      name: "selection-popup",
+      position: "overlay",
+      onMount(container, shadowRoot) {
+        const host = shadowRoot.host as HTMLElement;
+        Object.assign(host.style, {
+          position: "fixed",
+          top: "0",
+          left: "0",
+          width: "0",
+          height: "0",
+          overflow: "visible",
+          zIndex: "2147483647",
+          pointerEvents: "none",
+        });
+        const app = createApp(popupicon);
+        app.mount(container);
+        return app;
+      },
+      onRemove(app) {
+        app?.unmount();
+      },
+    });
+    ui.mount();
   },
 });
