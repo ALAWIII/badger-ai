2f00d89a28223ec0f86c43b726c1e26393cd3b40
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-28 19:20:41 +0300

    feat: attach and register some oh vue icons.

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -2,12 +2,19 @@
 import "@/assets/main.css";
 import { createApp } from "vue";
 import popupicon from "@/components/iconPopup.vue";
+import { LaHtml5, MdContentcopyRound } from "oh-vue-icons/icons";
+import { addIcons, OhVueIcon } from "oh-vue-icons";
 
+addIcons(LaHtml5);
+addIcons(MdContentcopyRound);
 export default defineContentScript({
   matches: ["<all_urls>"],
   main() {
     const container = document.createElement("div");
     document.body.appendChild(container);
-    createApp(popupicon).mount(container);
+
+    const app = createApp(popupicon);
+    app.component("v-icon", OhVueIcon);
+    app.mount(container);
   },
 });
