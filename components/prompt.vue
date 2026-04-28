91bacba52040fadd750f4b1d17f7701360483d8c
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-28 20:17:44 +0300

    feat: define key as property for the prompt.

--- components/prompt.vue
+++ components/prompt.vue
@@ -1,8 +1,12 @@
 <template>
-    <div class="flex flex-row w-full border border-white/30">
+    <div :key class="flex flex-row w-full border border-white/30">
         <span>prompt label</span>
         <div class="">
             <button><v-icon name="md-navigatenext-twotone" /></button>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+defineProps({ key: Number });
+</script>
