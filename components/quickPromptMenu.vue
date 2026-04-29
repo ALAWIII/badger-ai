0f4da668246ed5ce57a6097c67598f36b2c2821d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-29 17:25:02 +0300

    feat: update provider list container and fix textarea size properties..

--- components/quickPromptMenu.vue
+++ components/quickPromptMenu.vue
@@ -1,17 +1,19 @@
 <template>
     <div
         id="quick-menu"
-        class="flex flex-col fixed bg-black/80 h-80 w-60 z-2147483647 border border-white/20 rounded-2xl"
+        class="flex flex-col bg-black/80 h-80 w-60 border border-white/20 rounded-2xl p-2 min-h-fit"
     >
-        <div>
-            <h2>select provider</h2>
-            <div>
-                <input type="radio" />
-                <label>deepseek</label>
+        <div id="providers-list" class="flex-5/12 w-full gap-0.5">
+            <h3 class="text-center">select provider</h3>
+
+            <div class="w-full">
+                <input type="radio" class="mr-2" />
+                <label class="text-center">deepseek</label>
             </div>
         </div>
-        <div>
-            <textarea>
+        <div class="flex flex-col flex-7/12 gap-0.5 w-full">
+            <h3 class="flex-1/12 text-center">Quick Edit</h3>
+            <textarea class="flex-11/12 w-full outline-none">
                 {{ text }}
             </textarea>
         </div>
