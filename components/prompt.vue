ba1520f1abca057488413bf8207b380f8d5e615a
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-29 10:48:37 +0300

    feat: add prompt buttons ui components.

--- components/prompt.vue
+++ components/prompt.vue
@@ -1,12 +1,20 @@
 <template>
-    <div :key class="flex flex-row w-full border border-white/30">
-        <span>prompt label</span>
-        <div class="">
-            <button><v-icon name="md-navigatenext-twotone" /></button>
+    <div class="flex w-full items-center px-1 pb-0.5">
+        <div
+            class="flex-11/12 overflow-hidden border border-white/30 rounded-l-2xl hover:bg-amber-50/20"
+        >
+            <button
+                class="w-full h-full p-2 text-center text-nowrap text-white"
+            >
+                prompt label active only if the provider is none
+            </button>
+        </div>
+        <div
+            class="flex-1/12 ml-auto border border-white/30 rounded-r-xl hover:bg-amber-50/20"
+        >
+            <button class="p-2 w-full h-full text-white">
+                <v-icon name="md-navigatenext-twotone" />
+            </button>
         </div>
     </div>
 </template>
-
-<script setup lang="ts">
-defineProps({ key: Number });
-</script>
