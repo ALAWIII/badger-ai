d160731738907734d2228da044ef199dbea64ded
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-29 17:58:39 +0300

    feat: add click+ handlers to select text and html.

--- components/copyButtons.vue
+++ components/copyButtons.vue
@@ -8,7 +8,7 @@
             id="copy-text"
             class="bg-black rounded-l-2xl border text-white w-full h-full hover:bg-amber-50/20"
         >
-            <button class="w-full h-full">
+            <button class="w-full h-full" @click="selectText">
                 <v-icon name="md-contentcopy-round" class="h-[70%] w-[70%]" />
             </button>
         </div>
@@ -16,9 +16,18 @@
             id="copy-html"
             class="bg-black rounded-r-2xl border text-white w-full h-full hover:bg-amber-50/20"
         >
-            <button class="w-full h-full">
+            <button class="w-full h-full" @click="selectHtml">
                 <v-icon name="la-html5" class="h-[70%] w-[70%]" />
             </button>
         </div>
     </div>
 </template>
+<script setup lang="ts">
+const selectedContent = inject<Ref<string>>("selectedContent");
+function selectText() {
+    selectedContent!.value = getSelectedText();
+}
+function selectHtml() {
+    selectedContent!.value = getSelectedHtml();
+}
+</script>
