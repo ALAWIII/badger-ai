41d1a05ef3ba87a6f416e629f96ec883653f4692
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-30 21:26:21 +0300

    fix: id to their appropriate names.

--- entrypoints/manage/providers.vue
+++ entrypoints/manage/providers.vue
@@ -1,10 +1,10 @@
 <template>
     <div
-        id="prompts-list"
+        id="providers-list"
         class="flex-1 flex flex-col h-full bg-gray-800/50 rounded-2xl items-center p-2 gap-1"
     >
         <div
-            name="provider-header"
+            id="provider-header"
             class="flex flex-row w-full flex-1/12 text-center items-center border border-white/50 rounded-2xl"
         >
             <h2 class="flex-11/12">Providers</h2>
@@ -14,7 +14,7 @@
                 <v-icon name="bi-plus-circle-dotted" class="w-full h-full" />
             </div>
         </div>
-        <div name="list" class="flex-11/12 overflow-y-scroll w-full"></div>
+        <div id="prov-list" class="flex-11/12 overflow-y-scroll w-full"></div>
     </div>
 </template>
 <script setup lang="ts"></script>
