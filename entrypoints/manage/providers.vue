4944d907c7e8e54c4006dee68b2a61f75d7d1136
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-30 18:42:54 +0300

    feat: move components to their own files.

---
+++ entrypoints/manage/providers.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+        id="prompts-list"
+        class="flex-1 flex flex-col h-full bg-gray-800/50 rounded-2xl items-center p-2 gap-1"
+    >
+        <div
+            name="provider-header"
+            class="flex flex-row w-full flex-1/12 text-center items-center border border-white/50 rounded-2xl"
+        >
+            <h2 class="flex-11/12">Prompts</h2>
+            <div
+                class="flex-1/12 h-full w-full hover:bg-white/20 mr-auto rounded-r-2xl border-l border-white/50"
+            >
+                <v-icon name="bi-plus-circle-dotted" class="w-full h-full" />
+            </div>
+        </div>
+        <div class="flex-11/12"></div>
+    </div>
+</template>
+<script></script>
