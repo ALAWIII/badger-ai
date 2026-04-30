0b322c544de4daa3b3a6ba6efa83d7983fcd7cc9
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-30 19:32:56 +0300

    fix: switching sides and headers titles to the correct once.

--- entrypoints/manage/prompts.vue
+++ entrypoints/manage/prompts.vue
@@ -1,13 +1,13 @@
 <template>
     <div
-        id="providers-list"
+        id="prompts-list"
         class="flex-1 flex flex-col h-full bg-gray-800/50 rounded-2xl items-center p-2 gap-1"
     >
         <div
             name="provider-header"
             class="flex flex-row w-full flex-1/12 text-center items-center border border-white/50 rounded-2xl"
         >
-            <h2 class="flex-11/12">Providers</h2>
+            <h2 class="flex-11/12">Prompts</h2>
             <div
                 class="flex-1/12 h-full w-full active:bg-white/0 hover:bg-white/20 mr-auto rounded-r-2xl border-l border-white/50"
             >
--- entrypoints/manage/providers.vue
+++ entrypoints/manage/providers.vue
@@ -7,14 +7,14 @@
             name="provider-header"
             class="flex flex-row w-full flex-1/12 text-center items-center border border-white/50 rounded-2xl"
         >
-            <h2 class="flex-11/12">Prompts</h2>
+            <h2 class="flex-11/12">Providers</h2>
             <div
                 class="flex-1/12 h-full w-full active:bg-white/0 hover:bg-white/20 mr-auto rounded-r-2xl border-l border-white/50"
             >
                 <v-icon name="bi-plus-circle-dotted" class="w-full h-full" />
             </div>
         </div>
-        <div class="flex-11/12"></div>
+        <div name="list" class="flex-11/12 overflow-y-scroll w-full"></div>
     </div>
 </template>
 <script setup lang="ts"></script>
