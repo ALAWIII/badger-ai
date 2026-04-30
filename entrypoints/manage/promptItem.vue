05cf609eb1c0a8a12d48c1d97cc210041546735d
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-30 19:59:20 +0300

    feat: implement initial item.

---
+++ entrypoints/manage/promptItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div
+        class="flex flex-row justify-between text-center items-center w-full h-[10%] border border-white/50"
+    >
+        <div
+            name="drag-button"
+            class="flex-1/12 border-l-white/50 h-full hover:bg-white/10"
+        >
+            <v-icon name="fa-sort" class="text-green-500" />
+        </div>
+        <div name="prompt-label" class="flex-10/12"></div>
+        <div
+            name="remove-button"
+            class="flex-1/12 border-r-white/50 hover:bg-white/50"
+        >
+            <v-icon name="bi-trash" class="text-red-600" />
+        </div>
+    </div>
+</template>
+<script setup lang="ts"></script>
