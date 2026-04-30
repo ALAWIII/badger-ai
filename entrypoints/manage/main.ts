0ff874ef5d34cd38bb20c510f42b96520e1dd51e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-30 18:24:17 +0300

    feat: import main.css

--- entrypoints/manage/main.ts
+++ entrypoints/manage/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
 import ManagePage from "./manageUi.vue";
-
+import "@/assets/main.css";
 const optionPage = createApp(ManagePage);
 optionPage.mount("#manage-page");
