becc6de827011ed5957c3dc08a3dc9efe792c8b1
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-04-30 14:55:37 +0300

    feat: add BiPatchPlus icon

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -2,8 +2,8 @@ import { createApp } from "vue";
 import App from "./app.vue";
 import "@/assets/main.css";
 import { addIcons, OhVueIcon } from "oh-vue-icons";
-import { BiClipboardPlus } from "oh-vue-icons/icons";
-addIcons(BiClipboardPlus);
+import { BiPatchPlus } from "oh-vue-icons/icons";
+addIcons(BiPatchPlus);
 
 const app = createApp(App);
 app.component("v-icon", OhVueIcon);
