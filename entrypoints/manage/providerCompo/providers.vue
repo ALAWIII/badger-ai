54f0a506c9528037fa1a2446d1486ce1b76ebfed
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-05-01 19:38:42 +0300

    feat: finish implement provider storage ui functionality.

--- entrypoints/manage/providerCompo/providers.vue
+++ entrypoints/manage/providerCompo/providers.vue
@@ -10,16 +10,57 @@
             <h2 class="flex-11/12">Providers</h2>
             <div
                 class="flex-1/12 h-full w-full active:bg-white/0 hover:bg-white/20 mr-auto rounded-r-2xl border-l border-white/50"
+                @click="createNewProvider"
             >
                 <v-icon name="bi-plus-circle-dotted" class="w-full h-full" />
             </div>
         </div>
-        <div id="prov-list" class="flex-11/12 overflow-y-scroll w-full"></div>
+        <div
+            ref="provlist"
+            id="prov-list"
+            class="flex-11/12 overflow-y-scroll w-full"
+        >
+            <ProviderItem
+                v-for="p in providersList"
+                :key="p.id"
+                :provider="p"
+            />
+        </div>
+        <ProviderDialog v-model:dProvider="newProvider" />
     </div>
 </template>
 <script setup lang="ts">
 import { AIProvider } from "@/utils/models";
 import { provide } from "vue";
+import ProviderItem from "./providerItem.vue";
+import ProviderDialog from "./providerDialog.vue";
+import { getProviders, reorderProviders } from "@/utils/storage";
+import { v4 } from "uuid";
+import { useDraggable } from "vue-draggable-plus";
+
 const providersList = ref<AIProvider[]>([]);
-provide("providerList", providersList);
+provide("providersList", providersList);
+onMounted(async () => {
+    providersList.value = [...(await getProviders())];
+});
+const newProvider = ref<AIProvider | null>(null);
+function createNewProvider() {
+    const p: AIProvider = {
+        id: v4(),
+        apiKey: "",
+        name: "",
+        requestTemplate: "",
+        responsePath: "",
+        url: "",
+    };
+    newProvider.value = p;
+}
+const provlist = ref<HTMLElement | null>(null);
+const draggable = useDraggable(provlist, providersList, {
+    animation: 150,
+
+    async onUpdate() {
+        await reorderProviders(providersList.value);
+    },
+});
 </script>
