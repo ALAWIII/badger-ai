8d203bb29834d5fb4ca06f632b487e1919041349
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-05-01 15:01:48 +0300

    feat: provide access to storage,tabs.

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "wxt";
 import tailwindcss from "@tailwindcss/vite";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  manifest: {
+    permissions: ["storage", "tabs"],
+  },
   modules: ["@wxt-dev/module-vue"],
   vite: () => ({
     plugins: [tailwindcss()],
