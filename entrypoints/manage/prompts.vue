5f246d95d26163e455e7e720d9e84eeefd1c70a3
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-05-01 10:30:29 +0300

    feat: update div id, add testing item prompt to test the vue.

--- entrypoints/manage/prompts.vue
+++ entrypoints/manage/prompts.vue
@@ -4,7 +4,7 @@
         class="flex-1 flex flex-col h-full bg-gray-800/50 rounded-2xl items-center p-2 gap-1"
     >
         <div
-            name="provider-header"
+            id="prompts-header"
             class="flex flex-row w-full flex-1/12 text-center items-center border border-white/50 rounded-2xl"
         >
             <h2 class="flex-11/12">Prompts</h2>
@@ -14,7 +14,30 @@
                 <v-icon name="bi-plus-circle-dotted" class="w-full h-full" />
             </div>
         </div>
-        <div name="list" class="flex-11/12 overflow-y-scroll w-full"></div>
+        <div id="prom-list" class="flex-11/12 overflow-y-scroll w-full">
+            <PromptItem v-for="p in promptsList" :key="p.id" :prompt="p" />
+        </div>
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { getPrompts, addPrompt } from "@/utils/storage";
+import { Prompt } from "@/utils/models";
+import { ref, onMounted, type Ref } from "vue";
+import PromptItem from "./promptItem.vue";
+
+const promptsList = ref<Prompt[]>([
+    {
+        id: "lola",
+        label: "shawarma",
+        providerId: null,
+        systemPrompt: "kill",
+    },
+]);
+/*
+onMounted(async () => {
+    promptsList.value = [
+
+        ...(await getPrompts()),
+    ];
+    });*/
+</script>
