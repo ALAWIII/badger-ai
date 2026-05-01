070f92854261958445f70a763823e4f4a22fc24e
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-05-01 11:02:00 +0300

    feat: centering icons,buttons and texts, define props for the item card.

--- entrypoints/manage/promptItem.vue
+++ entrypoints/manage/promptItem.vue
@@ -1,20 +1,30 @@
 <template>
     <div
-        class="flex flex-row justify-between text-center items-center w-full h-[10%] border border-white/50"
+        class="flex flex-row justify-between text-center items-center w-full h-[10%] border border-white/50 rounded-2xl"
     >
         <div
             name="drag-button"
-            class="flex-1/12 border-l-white/50 h-full hover:bg-white/10"
+            class="flex-1/12 border-l-white/50 h-full hover:bg-white/10 rounded-l-2xl"
         >
-            <v-icon name="fa-sort" class="text-green-500" />
+            <v-icon name="fa-sort" class="h-full text-green-500" />
         </div>
-        <div name="prompt-label" class="flex-10/12"></div>
+        <div
+            name="prompt-label"
+            class="flex-10/12 h-full hover:bg-yellow-50/10 place-content-center"
+        >
+            {{ prompt?.label }}
+        </div>
         <div
             name="remove-button"
-            class="flex-1/12 border-r-white/50 hover:bg-white/50"
+            class="flex-1/12 border-r-white/50 hover:bg-white/10 h-full rounded-r-2xl"
         >
-            <v-icon name="bi-trash" class="text-red-600" />
+            <v-icon name="bi-trash" class="h-full text-red-600" />
         </div>
     </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Prompt } from "@/utils/models";
+defineProps<{
+    prompt: Prompt;
+}>();
+</script>
