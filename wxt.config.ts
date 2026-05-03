c4533fc774b57e248ca03998dd9d339e897ace3b
ALAWIII <60029291+ALAWIII@users.noreply.github.com>
2026-05-03 15:42:12 +0300

    feat: update project metadata.

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
+    name: "Badger AI",
+    short_name: "BadgerAI",
+    description:
+      "A browser extension that lets you run any AI prompt on selected text, with full control over providers and request templates. Configure your own API endpoints, models, and message structure — no vendor lock-in. ",
     web_accessible_resources: [
       {
         resources: ["icon.svg"],
